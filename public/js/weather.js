db1cb222bc27d8ba51a6b3e58b7fc9ee1cb7725c
TheAmeoba <theameoba@gmail.com>
2016-01-20 09:56:13 +1100

    Added SetInterval + confirmation prompt
    Defined function and added call to function with interval of 10000 milliseconds
    Added error checking dialog box to troubleshoot the running of the script

--- public/js/weather.js
+++ public/js/weather.js
@@ -1,5 +1,6 @@
         // Docs at http://simpleweatherjs.com
-        $(document).ready(function() {
+        var updateWeather = function () {
+            alert("Running weather function")
           $.simpleWeather({
             location: 'Philadephia, PA',
             woeid: '',
@@ -180,4 +181,8 @@
               $("#weather").html('<p>'+error+'</p>');
             }
           });
-        });
+        };
+        $(document).ready(function() {
+            updateWeather();
+            SetInterval(updateWeather, 10000);
+        };
